da68750ea73a99ca0b1cd873f8a0b98a58820d54
Tony McKendry <tonymckendry@gmail.com>
2016-03-08 16:01:30 -0700

    animation frustration

--- ang_app/app/js/controllers.js
+++ ang_app/app/js/controllers.js
@@ -8,10 +8,30 @@ app.controller("MainController", function($scope, $http, CatService){
   })
 
   app.controller("animator", function($scope){
-
+        $scope.developer = false;
+        $scope.designer = true;
         $scope.fade = true
     $scope.fader = function(){
       console.log('controller is working')
       $scope.fade = false
     }
+
+
+    var looper = function(){
+      setTimeout(looped, 2000)
+    }
+    var looped = function(){
+      $scope.developer = !$scope.developer;
+      $scope.designer = !$scope.designer;
+      looper()
+      console.log('looped')
+    }
+
+    $scope.looped = function(){
+      $scope.developer = !$scope.developer;
+      $scope.designer = !$scope.designer;
+      console.log('looped-click')
+    }
+
+    looper()
   })
